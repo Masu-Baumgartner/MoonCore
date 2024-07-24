24e8e7d5b8ff78654399140f306c15e332a01e0d
Baumgartner Marcel <marcel.kbkm@gmail.com>
2024-07-24 10:52:43 +0200

    Removed old display exception from core

--- MoonCore/MoonCore.Blazor.Bootstrap/Exceptions/DisplayException.cs
+++ MoonCore/MoonCore.Blazor.Bootstrap/Exceptions/DisplayException.cs
@@ -1,8 +1,5 @@
-﻿namespace MoonCore.Exceptions;
+﻿namespace MoonCore.Blazor.Bootstrap.Exceptions;
 
-/// <summary>
-/// This exception is used to trigger the ui showing an error message to the user and stop the current operation. The message shown to the user is the exception message
-/// </summary>
 public class DisplayException : Exception
 {
     public DisplayException()
