924aa9bdd5f24404d0f9cd79cf321c8de0673fde
Baumgartner Marcel <marcel.kbkm@gmail.com>
2024-07-24 10:57:11 +0200

    Added the original mapper from Mappy.net and optimized it a bit

---
+++ MoonCore/MoonCore/Helpers/Mapper.cs
@@ -0,0 +1,48 @@
+﻿namespace MoonCore.Helpers;
+
+public static class Mapper
+{
+    public static T Map<T>(object data)
+    {
+        var type = typeof(T);
+        var result = (T)Activator.CreateInstance(type)!;
+        
+        return Map<T>(result, data);
+    }
+
+    public static T Map<T>(T source, object data)
+    {
+        var type = typeof(T);
+        var result = source;
+
+        var targetProps = type
+            .GetProperties()
+            .ToDictionary(
+                x => x.Name
+                    .ToLower()
+                    .Replace("_", ""),
+                x => x
+            );
+
+        foreach (var prop in type.GetProperties())
+        {
+            var formattedName = prop.Name
+                .ToLower()
+                .Replace("_", "");
+
+            targetProps.Add(formattedName, prop);
+        }
+
+        foreach (var dataProp in data.GetType().GetProperties())
+        {
+            var formattedName = dataProp.Name
+                .ToLower()
+                .Replace("_", "");
+
+            if (targetProps.ContainsKey(formattedName))
+                targetProps[formattedName].SetValue(result, dataProp.GetValue(data));
+        }
+
+        return result;
+    }
+}
