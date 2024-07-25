45e2d98630cf8f790b12356e716dca0c5b220108
Baumgartner Marcel <marcel.kbkm@gmail.com>
2024-07-25 13:26:43 +0200

    Fixed mapper. Bumped up version

--- MoonCore/MoonCore/Helpers/Mapper.cs
+++ MoonCore/MoonCore/Helpers/Mapper.cs
@@ -1,4 +1,6 @@
-﻿namespace MoonCore.Helpers;
+﻿using System.Reflection;
+
+namespace MoonCore.Helpers;
 
 public static class Mapper
 {
@@ -6,8 +8,26 @@ public static class Mapper
     {
         var type = typeof(T);
         var result = (T)Activator.CreateInstance(type)!;
-        
-        return Map<T>(result, data);
+
+        Dictionary<string, PropertyInfo> targetProps = new();
+        foreach (var prop in type.GetProperties())
+        {
+            var formattedName = prop.Name.ToLower().Replace("_", "");
+            
+            targetProps[formattedName] = prop;
+        }
+
+        foreach (var dataProp in data.GetType().GetProperties())
+        {
+            var formattedName = dataProp.Name.ToLower().Replace("_", "");
+
+            if (targetProps.ContainsKey(formattedName))
+            {
+                targetProps[formattedName].SetValue(result, dataProp.GetValue(data));
+            }
+        }
+
+        return result;
     }
 
     public static T Map<T>(T source, object data)
@@ -15,32 +35,22 @@ public static class Mapper
         var type = typeof(T);
         var result = source;
 
-        var targetProps = type
-            .GetProperties()
-            .ToDictionary(
-                x => x.Name
-                    .ToLower()
-                    .Replace("_", ""),
-                x => x
-            );
-
+        Dictionary<string, PropertyInfo> targetProps = new();
         foreach (var prop in type.GetProperties())
         {
-            var formattedName = prop.Name
-                .ToLower()
-                .Replace("_", "");
-
-            targetProps.Add(formattedName, prop);
+            var formattedName = prop.Name.ToLower().Replace("_", "");
+            
+            targetProps[formattedName] = prop;
         }
 
         foreach (var dataProp in data.GetType().GetProperties())
         {
-            var formattedName = dataProp.Name
-                .ToLower()
-                .Replace("_", "");
+            var formattedName = dataProp.Name.ToLower().Replace("_", "");
 
             if (targetProps.ContainsKey(formattedName))
+            {
                 targetProps[formattedName].SetValue(result, dataProp.GetValue(data));
+            }
         }
 
         return result;
