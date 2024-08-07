a538c9492cdec95a35b9000d507d6f6fb2276014
Baumgartner Marcel <marcel.kbkm@gmail.com>
2024-08-07 18:04:45 +0200

    Started adding http client extensions

---
+++ MoonCore/MoonCore/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace MoonCore.Extensions;
+
+public static class HttpClientExtensions
+{
+    private 
+    
+    public static async Task<T> PostAsJsonReceiveJson<T>(string endpoint, object data)
+    {
+        
+    }
+}
