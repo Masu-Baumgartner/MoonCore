09b25deea4181170af6e414f27e78be96f9d2bf1
Masu-Baumgartner <68913099+Masu-Baumgartner@users.noreply.github.com>
2024-10-01 10:25:19 +0200

    Extracted plugin framework to seperate mooncore library

---
+++ MoonCore/MoonCore.PluginFramework/Abstractions/BasePlugin.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MoonCore.PluginFramework.Abstractions;
+
+public abstract class BasePlugin
+{
+    public ILogger<BasePlugin> Logger { get; set; }
+    
+    protected BasePlugin(ILogger<BasePlugin> logger)
+    {
+        Logger = logger;
+    }
+}
---
+++ MoonCore/MoonCore.PluginFramework/Services/ImplementationService.cs
@@ -0,0 +1,49 @@
+﻿namespace MoonCore.PluginFramework.Services;
+
+public class ImplementationService
+{
+    private readonly Dictionary<Type, List<object>> Implementations = new();
+    
+    public void Register<T>(T implementation) where T : class
+    {
+        var interfaceType = typeof(T);
+
+        lock (Implementations)
+        {
+            if(!Implementations.ContainsKey(interfaceType))
+                Implementations.Add(interfaceType, new());
+
+            Implementations[interfaceType].Add(implementation);
+        }
+    }
+
+    public void Register<T, TImpl>() where TImpl : T where T : class
+    {
+        var impl = Activator.CreateInstance<TImpl>();
+
+        Register<T>(impl);
+    }
+
+    public T[] Get<T>() where T : class
+    {
+        var interfaceType = typeof(T);
+
+        lock (Implementations)
+        {
+            if (!Implementations.TryGetValue(interfaceType, out var implementation))
+                return [];
+
+            return implementation
+                .Select(x => (T)x)
+                .ToArray();
+        }
+    }
+
+    public async Task Execute<T>(Func<T, Task> func) where T : class
+    {
+        var implementations = Get<T>();
+
+        foreach (var implementation in implementations)
+            await func.Invoke(implementation);
+    }
+}
