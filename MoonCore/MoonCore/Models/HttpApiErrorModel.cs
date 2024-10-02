96b563e29a551c20942c0caabc0269a8e2a9a036
Masu-Baumgartner <68913099+Masu-Baumgartner@users.noreply.github.com>
2024-10-02 16:25:39 +0200

    Allowed some values of the exception model to be null and passed on to the exception

--- MoonCore/MoonCore/Models/HttpApiErrorModel.cs
+++ MoonCore/MoonCore/Models/HttpApiErrorModel.cs
@@ -4,6 +4,6 @@ public class HttpApiErrorModel
 {
     public string Title { get; set; } = "";
     public int Status { get; set; }
-    public string Detail { get; set; } = "";
-    public Dictionary<string, List<string>> Errors { get; set; } = new();
+    public string? Detail { get; set; }
+    public Dictionary<string, List<string>>? Errors { get; set; }
 }
