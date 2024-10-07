b46f0b7a07ca77f6a35ec73e0effb616e0306668
Masu-Baumgartner <68913099+Masu-Baumgartner@users.noreply.github.com>
2024-10-07 16:34:20 +0200

    Fixed some smaller issues. Bumped up version

--- MoonCore/MoonCore.Blazor.Tailwind/Attributes/RequirePermissionAttribute.cs
+++ MoonCore/MoonCore.Blazor.Tailwind/Attributes/RequirePermissionAttribute.cs
@@ -3,4 +3,9 @@
 public class RequirePermissionAttribute : Attribute
 {
     public string Permission { get; set; }
+    
+    public RequirePermissionAttribute(string permission)
+    {
+        Permission = permission;
+    }
 }
