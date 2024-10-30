7d234ee79cbb12fa5861e4253eccc09318c3afe4
Masu-Baumgartner <68913099+Masu-Baumgartner@users.noreply.github.com>
2024-10-30 13:02:52 +0100

    Removed legacy startup system. Added claims principle extensions. Moved required permission attribute

---
+++ MoonCore/MoonCore/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Claims;
+using MoonCore.Authentication;
+using MoonCore.Helpers;
+
+namespace MoonCore.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static bool HasPermission(this ClaimsPrincipal claimsPrincipal, string permission)
+    {
+        if (claimsPrincipal is not PermClaimsPrinciple permClaimsPrinciple)
+            return false;
+
+        return PermissionHelper.HasPermissions(permClaimsPrinciple.Permissions, permission);
+    }
+    
+    public static bool HasPermissions(this ClaimsPrincipal claimsPrincipal, string[] permissions)
+    {
+        if (claimsPrincipal is not PermClaimsPrinciple permClaimsPrinciple)
+            return false;
+
+        foreach (var permission in permissions)
+        {
+            if (!PermissionHelper.HasPermissions(permClaimsPrinciple.Permissions, permission))
+                return false;
+        }
+
+        return true;
+    }
+
+    public static T AsIdentity<T>(this ClaimsPrincipal claimsPrincipal)
+    {
+        if (claimsPrincipal is not PermClaimsPrinciple permClaimsPrinciple)
+            throw new ArgumentException("The ClaimsPrincipal is not a PermClaimsPrinciple");
+
+        return (T)permClaimsPrinciple.IdentityModel;
+    }
+}
