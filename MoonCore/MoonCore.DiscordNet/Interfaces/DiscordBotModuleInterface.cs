675daa78be713e8853dc2fc9259c4782d5957537
Ole Sziedat <info@spielepapagei.de>
2024-11-02 21:00:02 +0100

    added Bot Interface

---
+++ MoonCore/MoonCore.DiscordNet/Interfaces/DiscordBotModuleInterface.cs
@@ -0,0 +1,19 @@
+﻿namespace MoonCore.DiscordNet.Interfaces;
+
+public interface DiscordBotModuleInterface
+{
+    /// <summary>
+    /// This is an (async) implementation to Load EventHandlers
+    /// </summary>
+    public Task InitializeAsync();
+
+    /// <summary>
+    /// This is an (async) implementation to Unload EventHandlers
+    /// </summary>
+    public Task UnloadAsync();
+    
+    /// <summary>
+    /// This is an (async) implementation to Registering for Example SlashCommands
+    /// </summary>
+    public Task RegisterAsync();
+}
