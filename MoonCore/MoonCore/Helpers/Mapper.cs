905780f0eeb192941f69455f24300965c857ee9a
Masu-Baumgartner <68913099+Masu-Baumgartner@users.noreply.github.com>
2024-11-13 09:42:05 +0100

    Improved mapper error handling

--- MoonCore/MoonCore/Helpers/Mapper.cs
+++ MoonCore/MoonCore/Helpers/Mapper.cs
@@ -1,10 +1,11 @@
 using System.Reflection;
+using Microsoft.Extensions.Logging;
 
 namespace MoonCore.Helpers;
 
 public static class Mapper
 {
-    public static object MapRaw(object source, object data, bool ignoreNullValues = false)
+    public static object MapRaw(object source, object data, bool ignoreNullValues = false, ILogger? logger = null)
     {
         var sourceProperties = new Dictionary<string, PropertyInfo>();
 
@@ -18,15 +19,22 @@ public static class Mapper
 
         foreach (var dataProperty in dataProperties)
         {
-            if(!sourceProperties.TryGetValue(dataProperty.Key, out var sourceProperty))
-                continue;
-            
-            var value = dataProperty.Value.GetValue(data);
+            try
+            {
+                if (!sourceProperties.TryGetValue(dataProperty.Key, out var sourceProperty))
+                    continue;
 
-            if (ignoreNullValues && value == null)
-                continue;
+                var value = dataProperty.Value.GetValue(data);
 
-            sourceProperty.SetValue(source, value);
+                if (ignoreNullValues && value == null)
+                    continue;
+
+                sourceProperty.SetValue(source, value);
+            }
+            catch (Exception e)
+            {
+                logger?.LogWarning("Unable to map property {name}: {e}", dataProperty.Key, e);
+            }
         }
 
         return source;
@@ -34,13 +42,13 @@ public static class Mapper
 
     #region Object creating mappers
 
-    public static object Map(Type type, object data, bool ignoreNullValues = false) => MapRaw(Activator.CreateInstance(type)!, data, ignoreNullValues: ignoreNullValues);
+    public static object Map(Type type, object data, bool ignoreNullValues = false, ILogger? logger = null) => MapRaw(Activator.CreateInstance(type)!, data, ignoreNullValues, logger);
 
-    public static T Map<T>(object data, bool ignoreNullValues = false) => (T)Map(typeof(T), data, ignoreNullValues: ignoreNullValues);
+    public static T Map<T>(object data, bool ignoreNullValues = false, ILogger? logger = null) => (T)Map(typeof(T), data, ignoreNullValues, logger);
 
     #endregion
 
-    public static T Map<T>(T source, object data, bool ignoreNullValues = false) => (T)MapRaw(source!, data, ignoreNullValues: ignoreNullValues);
+    public static T Map<T>(T source, object data, bool ignoreNullValues = false, ILogger? logger = null) => (T)MapRaw(source!, data, ignoreNullValues, logger);
 
     private static string BuildId(string name) => name
         .Replace(".", "")
