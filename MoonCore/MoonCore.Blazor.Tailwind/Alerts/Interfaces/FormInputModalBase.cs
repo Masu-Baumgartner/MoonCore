f8ad21db9de6ebcf434e0ea30bfd166d9f9135ce
Moritz <101179677+mxritzdev@users.noreply.github.com>
2024-11-22 15:50:10 +0100

    fixed forminput
    fixed stupid error i made when i first wrote it, now it works as expected

---
+++ MoonCore/MoonCore.Blazor.Tailwind/Alerts/Interfaces/FormInputModalBase.cs
@@ -0,0 +1,8 @@
+﻿using MoonCore.Blazor.Tailwind.Modals.Components;
+
+namespace MoonCore.Blazor.Tailwind.Alerts.Interfaces;
+
+public abstract class FormInputModalBase<T> : BaseModal where T : class
+{
+    public abstract Task RegisterFormInputUi(FormInputUi<T> formInputUi);
+}
--- MoonCore/MoonCore.Blazor.Tailwind/Alerts/Interfaces/FormInputUi.cs
+++ MoonCore/MoonCore.Blazor.Tailwind/Alerts/Interfaces/FormInputUi.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MoonCore.Blazor.Tailwind.Alerts.Components;
 
 namespace MoonCore.Blazor.Tailwind.Alerts.Interfaces;
 
 public abstract class FormInputUi<T> : ComponentBase, IFormUiEvaluateable<T> where T : class
 {
+    [CascadingParameter]
+    public FormInputModalBase<T> FormInputAlert { get; set; }
+    
     /// <summary>
     /// Evaluates the form input and returns an instance of type T if the input is valid.
-    /// It should only evaluate the input. It cannot not change the state of the component.
+    /// It should only evaluate the input. If the input is invalid, it should return null, this should be handled by the caller.
     /// </summary>
     public abstract Task<T?> Evaluate();
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender)
+            return;
+        
+        if (FormInputAlert == null)
+        {
+            throw new InvalidOperationException("FormInputUi must be a child of FormInputAlert");
+        }
+
+        await FormInputAlert.RegisterFormInputUi(this);
+    }
 }
