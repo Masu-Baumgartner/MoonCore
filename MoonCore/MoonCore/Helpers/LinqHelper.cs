f3b729a6c9e76f461bddf70dda3ffd938c785565
ChiaraBm <chiara.bm04@gmail.com>
2024-12-03 15:09:49 +0100

    Implementing DataTable and filtering/sorting

---
+++ MoonCore/MoonCore/Helpers/LinqHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace MoonCore.Helpers;
+
+public static class LinqHelper
+{
+    public static PropertyInfo GetPropertyInfo<T>(Expression<Func<T, object?>> expression)
+    {
+        MemberExpression? memberExpression = default;
+
+        if (expression.Body is MemberExpression body)
+            memberExpression = body;
+        else if (expression.Body is UnaryExpression unaryExpression &&
+                 unaryExpression.NodeType == ExpressionType.Convert)
+        {
+            if (unaryExpression.Operand is MemberExpression operand)
+                memberExpression = operand;
+        }
+
+        if (memberExpression == null)
+            throw new ArgumentException("Unable to process expression");
+
+        if (memberExpression.Member is PropertyInfo propertyInfo)
+        {
+            return propertyInfo;
+        }
+
+        throw new ArgumentException("Expression does not represent a property.");
+    }
+}
