299c8cc8e266f1f69c91333e66f24163144555dc
ChiaraBm <chiara.bm04@gmail.com>
2025-01-17 10:41:30 +0100

    Added rename actions. Implemented delete, rename and move

--- MoonCore/MoonCore.Blazor.Tailwind.Test/UI/Fm/HostFileSystemProvider.cs
+++ MoonCore/MoonCore.Blazor.Tailwind.Test/UI/Fm/HostFileSystemProvider.cs
@@ -14,13 +14,13 @@ public class HostFileSystemProvider : IFileSystemProvider
     public async Task<FileSystemEntry[]> List(string path)
     {
         var entries = new List<FileSystemEntry>();
-        
+
         var files = Directory.GetFiles(PathBuilder.Dir(BaseDirectory, path));
 
         foreach (var file in files)
         {
             var fi = new FileInfo(file);
-            
+
             entries.Add(new FileSystemEntry()
             {
                 Name = fi.Name,
@@ -36,7 +36,7 @@ public class HostFileSystemProvider : IFileSystemProvider
         foreach (var directory in directories)
         {
             var di = new DirectoryInfo(directory);
-            
+
             entries.Add(new FileSystemEntry()
             {
                 Name = di.Name,
@@ -53,26 +53,46 @@ public class HostFileSystemProvider : IFileSystemProvider
     public async Task Create(string path, Stream stream)
     {
         var baseDir = Path.GetDirectoryName(path);
-        
-        if(!string.IsNullOrEmpty(baseDir))
+
+        if (!string.IsNullOrEmpty(baseDir))
             Directory.CreateDirectory(PathBuilder.Dir(BaseDirectory, baseDir));
 
         await using var fs = File.Create(PathBuilder.File(BaseDirectory, path));
-        
+
         await stream.CopyToAsync(fs);
-        
+
         await fs.FlushAsync();
         fs.Close();
     }
 
     public Task Move(string oldPath, string newPath)
     {
-        throw new NotImplementedException();
+        if (Directory.Exists(PathBuilder.Dir(BaseDirectory, oldPath)))
+        {
+            Directory.Move(
+                PathBuilder.Dir(BaseDirectory, oldPath),
+                PathBuilder.Dir(BaseDirectory, newPath)
+            );
+        }
+        else
+        {
+            File.Move(
+                PathBuilder.File(BaseDirectory, oldPath),
+                PathBuilder.File(BaseDirectory, newPath)
+            );
+        }
+
+        return Task.CompletedTask;
     }
 
     public Task Delete(string path)
     {
-        throw new NotImplementedException();
+        if (Directory.Exists(PathBuilder.Dir(BaseDirectory, path)))
+            Directory.Delete(PathBuilder.Dir(BaseDirectory, path), true);
+        else
+            File.Delete(PathBuilder.File(BaseDirectory, path));
+
+        return Task.CompletedTask;
     }
 
     public Task CreateDirectory(string path)
