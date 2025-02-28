a156e6fabaceba0983bdd6b6ce4d65010a982ad3
ChiaraBm <chiara.bm04@gmail.com>
2025-02-28 09:03:30 +0100

    Removed legacy plugin system. Changed version

--- MoonCore/MoonCore/Plugins/IPluginSource.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.Loader;
-
-namespace MoonCore.Plugins;
-
-public interface IPluginSource
-{
-    public Task Load(AssemblyLoadContext loadContext, List<string> entrypoints);
-}
