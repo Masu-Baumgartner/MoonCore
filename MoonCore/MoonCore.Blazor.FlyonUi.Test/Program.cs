2cfb72da392354f8d1a73914ef152ce77f72d39e
ChiaraBm <chiara.bm04@gmail.com>
2025-06-16 01:51:43 +0200

    Ported application router and crash handler. Improved dropdown

--- MoonCore/MoonCore.Blazor.FlyonUi.Test/Program.cs
+++ MoonCore/MoonCore.Blazor.FlyonUi.Test/Program.cs
@@ -1,12 +1,16 @@
 using MoonCore.Blazor.FlyonUi.Test.UI;
 using MoonCore.Blazor.FlyonUi.Auth;
 using MoonCore.Blazor.FlyonUi.Test;
+using MoonCore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddMoonCore();
 
 builder.Services.AddAuthenticationStateManager<CoolestAuthStateManager>();
 
