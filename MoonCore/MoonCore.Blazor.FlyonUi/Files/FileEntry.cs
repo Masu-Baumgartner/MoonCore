22466cb510b3aba3b2538f318cbf4e91eb8bf094
ChiaraBm <chiara.bm04@gmail.com>
2025-06-18 17:00:40 +0200

    Started designing file manager and file view for flyonui

---
+++ MoonCore/MoonCore.Blazor.FlyonUi/Files/FileEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace MoonCore.Blazor.FlyonUi.Files;
+
+public class FileEntry
+{
+    public string Name { get; set; }
+    public long Size { get; set; }
+    public bool IsFolder { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ MoonCore/MoonCore.Blazor.FlyonUi/Files/Manager/IFileAccess.cs
@@ -0,0 +1,18 @@
+﻿namespace MoonCore.Blazor.FlyonUi.Files.Manager;
+
+public interface IFileAccess
+{
+    public Task CreateFile(string path);
+    public Task CreateDirectory(string path);
+
+    public Task<FileEntry[]> List(string path);
+    public Task Move(string oldPath, string newPath);
+
+    public Task<Stream> Read(string path);
+    public Task Write(string path, Stream dataStream);
+
+    public Task Delete(string path);
+
+    public Task Upload(Func<int, Task> updateProgress, string path, Stream dataStream);
+    public Task Download(Func<int, Task> updateProgress, string path, string fileName);
+}
