258900ab58c36d5f84518f674c8578c9a9ca56a5
ChiaraBm <chiara.bm04@gmail.com>
2025-06-29 18:08:30 +0200

    Improved context menu. Added demo page to navigation

--- MoonCore/MoonCore.Blazor.FlyonUi.Test/UI/UiConstants.cs
+++ MoonCore/MoonCore.Blazor.FlyonUi.Test/UI/UiConstants.cs
@@ -2,9 +2,9 @@ namespace MoonCore.Blazor.FlyonUi.Test.UI;
 
 public static class UiConstants
 {
-    public static readonly string[] ComponentNames = ["Buttons", "Lazy Loaders", "Tabs", "Dropdowns"];
-    public static readonly string[] ComponentLinks = ["/components", "/components/lazyloaders", "/components/tabs", "/components/dropdowns"];
-    public static readonly string[] ComponentIcons = ["icon-circle-power", "icon-loader-circle", "icon-layers-2", "icon-chart-bar-decreasing"];
+    public static readonly string[] ComponentNames = ["Buttons", "Lazy Loaders", "Tabs", "Dropdowns", "Context Menu"];
+    public static readonly string[] ComponentLinks = ["/components", "/components/lazyloaders", "/components/tabs", "/components/dropdowns", "/components/contextmenu"];
+    public static readonly string[] ComponentIcons = ["icon-circle-power", "icon-loader-circle", "icon-layers-2", "icon-chart-bar-decreasing", "icon-square-menu"];
     
     public static readonly string[] DataTablesNames = ["Default", "Pagination", "Selection", "Loading"];
     public static readonly string[] DataTablesLinks = ["/datatables", "/datatables/pagination", "/datatables/selection", "/datatables/loading"];
