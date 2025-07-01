498f279d6839f2011f9bce54beb15011682edd92
ChiaraBm <chiara.bm04@gmail.com>
2025-07-01 16:17:06 +0200

    Added context menu to file manager. Started implementing yaml configuration extension

---
+++ MoonCore/MoonCore/YamlConfiguration/YamlCommentAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace MoonCore.YamlConfiguration;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class YamlCommentAttribute : Attribute
+{
+    public string Comment { get; }
+
+    public YamlCommentAttribute(string comment)
+    {
+        Comment = comment;
+    }
+}
---
+++ MoonCore/MoonCore/YamlConfiguration/YamlConfigurationSource.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MoonCore.YamlConfiguration;
+
+public class YamlConfigurationSource : IConfigurationSource
+{
+    private readonly string Path;
+    private readonly bool IsOptional;
+
+    public YamlConfigurationSource(string path, bool isOptional)
+    {
+        Path = path;
+        IsOptional = isOptional;
+    }
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new YamlConfigurationProvider(Path, IsOptional);
+    }
+}
