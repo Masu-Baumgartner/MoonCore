5364ba1eb0a9d8c8f0452f1a793735fba882553f
ChiaraBm <chiara.bm04@gmail.com>
2025-08-20 23:44:23 +0200

    Started implementing file open handling for file manager

---
+++ MoonCore/MoonCore.Blazor.FlyonUi/Files/Manager/FileManager.Open.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MoonCore.Blazor.FlyonUi.Files.Manager;
+
+public partial class FileManager
+{
+    private RenderFragment? OpenWindow;
+    
+    private async Task Open(FsEntry entry)
+    {
+        var openOperation = Options
+            .OpenOperations
+            .FirstOrDefault(x => x.Filter.Invoke(entry));
+
+        if (openOperation == null)
+        {
+            await ToastService.Error("Cannot open the file: Not supported");
+            return;
+        }
+
+        if (Options.OpenLimit != -1 && entry.Size > Options.OpenLimit)
+        {
+            await ToastService.Error("Cannot open the file: Exceeded the open limit");
+            return;
+        }
+        
+        var pwd = new string(CurrentPath);
+
+        OpenWindow = await openOperation.Open(
+            pwd,
+            entry,
+            FsAccess,
+            this
+        );
+
+        await InvokeAsync(StateHasChanged);
+    }
+    
+    public async Task CloseOpenScreen()
+    {
+        OpenWindow = null;
+        await InvokeAsync(StateHasChanged);
+    }
+}
