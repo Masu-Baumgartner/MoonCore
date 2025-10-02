a135af67cd0af798a9f078836019a1e5557db7c9
ChiaraBm <chiara.bm04@gmail.com>
2025-10-02 10:51:12 +0200

    Added sync variant of yaml default generators generate function

--- MoonCore/MoonCore/Yaml/YamlDefaultGenerator.cs
+++ MoonCore/MoonCore/Yaml/YamlDefaultGenerator.cs
@@ -26,4 +26,26 @@ public static class YamlDefaultGenerator
         var newYaml = YamlSerializer.Serialize(model);
         await File.WriteAllTextAsync(path, newYaml);
     }
+    
+    /// <summary>
+    /// Generates a yaml file in a specific scheme using the defined default values.
+    /// If a file already exists it updates 
+    /// </summary>
+    /// <param name="path">Path of the yaml file</param>
+    /// <typeparam name="T">Type of the scheme</typeparam>
+    public static void Generate<T>(string path)
+    {
+        T model;
+
+        if (File.Exists(path))
+        {
+            var yaml = File.ReadAllText(path);
+            model = YamlSerializer.Deserialize<T>(yaml);
+        }
+        else
+            model = Activator.CreateInstance<T>();
+
+        var newYaml = YamlSerializer.Serialize(model);
+        File.WriteAllText(path, newYaml);
+    }
 }
